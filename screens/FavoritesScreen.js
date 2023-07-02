ffb3305d2f5bb123cac2038d7988b0e7b7fb43fa
Hessam <hessam.imen@gmail.com>
2023-07-02 20:24:41 +1000

    feat: manage the Favorite screen when there is no favorite meal chosen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import MealsList from "../components/MealsList/MealsList";
+import { View, Text, StyleSheet } from "react-native";
 
 function FavoritesScreen() {
   const favoriteMealsCtx = useContext(FavoritesContext);
@@ -10,7 +11,28 @@ function FavoritesScreen() {
     favoriteMealsCtx.ids.includes(meal.id)
   );
 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
