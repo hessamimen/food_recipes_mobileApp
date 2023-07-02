5c5dd5997e10080903c8222c0310a7275a0d5b6c
Hessam <hessam.imen@gmail.com>
2023-07-02 20:10:28 +1000

    feat: create the MealList component to render the favorite meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import MealsList from "../components/MealsList/MealsList";
 
 function FavoritesScreen() {
-  return <Text>The favourite screen</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
